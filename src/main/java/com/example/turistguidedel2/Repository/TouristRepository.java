cababd5e801906ecd5d5cb4ef0df599fa6730029
MH020 <166133291+MH020@users.noreply.github.com>
2024-10-12 12:29:35 +0200

    application profil tilføjet og lacal database forbindelse etableret

--- src/main/java/com/example/turistguidedel2/Repository/TouristRepository.java
+++ src/main/java/com/example/turistguidedel2/Repository/TouristRepository.java
@@ -1,12 +1,22 @@
 package com.example.turistguidedel2.Repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import com.example.turistguidedel2.Model.TouristAttraction;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class TouristRepository {
+    @Value("${TEST_DATABASE_URL}")
+    private String databaseUrl;
+    @Value("${TEST_USERNAME}")
+    private String username;
+    @Value("${TEST_PASSWORD}")
+    private String password;
+
     // this is a list of tourist attractions that will be used to store the tourist attractions
     private  ArrayList<TouristAttraction> touristAttractions = new ArrayList<>(); //Using a map (name, desc) could be beneficial.
     private ArrayList<String> tags = new ArrayList<String>();
@@ -79,10 +89,31 @@ public class TouristRepository {
 
 
     public void saveTouristAttractions(TouristAttraction touristAttraction){
+        try (Connection conn = DriverManager.getConnection(databaseUrl, username, password)) {
+            System.out.println("Connected to the database");
+            Statement st = conn.createStatement();
+            String sql = "SELECT * FROM touristAttraction";
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String city = rs.getString("city");
+                String tags = rs.getString("tags");
+                System.out.println(name + " " + city + " " + tags);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("failed to connect to the database");
+        }
         touristAttractions.add(touristAttraction);
 }
 
-
+    public void connectToDatabase() {
+        try (Connection conn = DriverManager.getConnection(databaseUrl, username, password)) {
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 
