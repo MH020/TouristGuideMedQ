0e75bd82a7918c91b7ba1300c86a3d97d687331c
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-10-15 14:29:58 +0200

    Create ConnectionManager.java

---
+++ src/main/java/com/example/turistguidedel2/Repository/ConnectionManager.java
@@ -0,0 +1,33 @@
+package com.example.turistguidedel2.Repository;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.*;
+
+public class ConnectionManager {
+    static Connection connection;
+
+    @Value("${TEST_DATABASE_URL}")
+    private String databaseUrl;
+    @Value("${TEST_USERNAME}")
+    private String username;
+    @Value("${TEST_PASSWORD}")
+    private String password;
+
+    private ConnectionManager(){
+        instantiateConnection();
+    }
+
+    private void instantiateConnection(){
+        if(connection != null){
+            System.out.println("hej");
+        }else{
+            try {
+                connection = DriverManager.getConnection(databaseUrl, username, password);
+                System.out.println("Connected to the database");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failed to connect to the database");
+            }
+        }
+    }
+}
