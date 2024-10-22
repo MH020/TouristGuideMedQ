6a5af2924be12b8e0ffc8d53c15041402d0df7fa
jazzyzoney <jaqtash22@gmail.com>
2024-10-22 14:59:00 +0200

    empty tests

---
+++ src/test/java/com/example/turistguidedel2/Model/TouristAttractionTest.java
@@ -0,0 +1,40 @@
+package com.example.turistguidedel2.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouristAttractionTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getTags() {
+    }
+
+    @Test
+    void setTags() {
+    }
+
+    @Test
+    void getCity() {
+    }
+
+    @Test
+    void setCity() {
+    }
+}
---
+++ src/test/java/com/example/turistguidedel2/Service/TouristServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.turistguidedel2.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouristServiceTest {
+
+    @Test
+    void getAllTouristAttractions() {
+    }
+
+    @Test
+    void updateTouristAttraction() {
+    }
+
+    @Test
+    void deleteTouristAttraction() {
+    }
+
+    @Test
+    void getTouristAttractionByName() {
+    }
+
+    @Test
+    void getAllTags() {
+    }
+
+    @Test
+    void saveTouristAttractions() {
+    }
+}
