2b3d06d88a38d01129af6a87653b986527cbd7fc
jazzyzoney <jaqtash22@gmail.com>
2024-10-28 16:04:47 +0100

    rettelser

--- src/test/java/com/example/turistguidedel2/Controller/TouristControllerIT.java
+++ src/test/java/com/example/turistguidedel2/Controller/TouristControllerIT.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 //@Transactional
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-prod.properties")
 public class TouristControllerIT {
 
     @Autowired
